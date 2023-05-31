57b9311527587c157e743ccee0da85474d4f13ad
411017284 <s1101728@gm.pu.edu.tw>
2023-05-31 11:03:56 +0800

    v.1

--- app/src/main/java/com/example/ruby/MainActivity.kt
+++ app/src/main/java/com/example/ruby/MainActivity.kt
@@ -3,16 +3,22 @@ package com.example.ruby
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ruby.ui.theme.RubyTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,23 +40,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally)
-    {
-        Text(text = "作者:資管二A 陳柔涵")
-        Image(
-            painter = painterResource(id = R.drawable.map),
-            contentDescription = "")
-    }
+    Text(text = "Hello Android!")
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RubyTheme {
-        Greeting("Android")
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        RubyTheme {
+            Greeting("Android")
+        }
     }
-}
